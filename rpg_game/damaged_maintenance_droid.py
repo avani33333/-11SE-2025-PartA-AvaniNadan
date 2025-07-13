1977076c0df037ebb136ffe9dfcfdf569303d1a4
avani33333 <anadan@students.rj.nsw.edu.au>
2025-07-13 13:20:18 +1000

    Update and rename DamagedMaintenanceDroid.py to damaged_maintenance_droid.py

--- rpg_game/damaged_maintenance_droid.py
+++ rpg_game/damaged_maintenance_droid.py
@@ -17,7 +17,7 @@ class DamagedMaintenanceDroid:
     def show_blocked_message(self, add_hazard=True):
         print("The droid is blocking the way.")
         if add_hazard:
-            print("+1 hazard")
+            print("\033[34m+1 hazard\033[0m")
         print("------------------------------------")
         print("Location: Maintenance Tunnel")
         print("\x1B[3mEnter: Use Diagnostic Tool to disable\x1B[0m")
@@ -39,8 +39,8 @@ class DamagedMaintenanceDroid:
         self.is_disabled = True
         self.is_blocking = False
         self.diagnostic_attempted = True
-        return ("Attempting reboot...\n"
-                "Reboot successful!\n"
+        return ("\n\033[34mAttempting reboot...\033[0m"
+                "\n\033Reboot successful!\033[0m"
                 "Mobility partially restored.")
     
     def is_path_clear(self):
@@ -48,19 +48,19 @@ class DamagedMaintenanceDroid:
     
     def use_tool(self, run_test, found=True):
         if run_test == "1" and found == True:
-            return "[Running Diagnostic Scan... ███████░░░ 70%]\nError Code: M-43X Detected\nStatus: Droid mobility subroutine malfunctioning."
+            return "\033[34m[Running Diagnostic Scan... ███████░░░ 70%]\nError Code: M-43X Detected\nStatus: Droid mobility subroutine malfunctioning.\033[0m"
         elif run_test == "1" and found == False:
             return "You do not own a diagnostic tool. You need to find one to proceed."
         elif run_test == "2" and found == True:
             return "The droid is blocking the way."
     
     def droid_faults(self, run_test=1, found=True):
-        return ("\033[1mDiagnostic Complete.\033[0m\n"
-                "Detected Faults:\n"
-                "- Motor Controller Failure\n"
-                "- Sensor Loop Error"
+        return ("\033[1mDiagnostic Complete.\033[0m"
+                "\n\033[94mDetected Faults:"
+                "\n -Motor Controller Failure"
+                "\n -Sensor Loop Error\033[0m"
                 "\n------------------------------------\n"
-                "Location: Maintenance Tunnel\n"
+                "\n\033[1mLocation: Maintenance Tunnel\033[0m"
                 "1. Attempt to reboot motor controller\n"
                 "2. Bypass sensor loop\n"
                 "3. Exit diagnostic")
@@ -70,8 +70,8 @@ class DamagedMaintenanceDroid:
             self.is_blocking = False
             self.is_disabled = True
             self.diagnostic_attempted = True
-            return ("Attempting reboot...\n"
-                   "Reboot successful!\n"
+            return ("\n\033[34mAttempting reboot...\033[0m"
+                   "\n\033[34mReboot successful!\033[0m"
                    "Mobility partially restored.")
         elif option_repair == "2":
             return "Bypass failed. You must fix the motor controller first."
