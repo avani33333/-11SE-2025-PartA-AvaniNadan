6fa0a000453ba510486b745853aa18be34e681ac
avani33333 <anadan@students.rj.nsw.edu.au>
2025-08-27 20:14:56 +1000

    Update station_item.py

--- rpg_game/station_item.py
+++ rpg_game/station_item.py
@@ -1,10 +1,12 @@
 class StationItem:
+    """Parent class for energy crystal and diagnostic tool."""
     def __init__(self, name, description, usable=True):
         self.name = name
         self.description = description
-        self.usable = usable
+        self.usable = usable  # Indicates if the item can be used.
     
     def use(self):
+        
         if self.usable:
             return f"You use the {self.name}."
         else:
